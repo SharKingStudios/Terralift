204e814f3215eead70fcecbb19d18473b110499d
Logan Peterson <112569974+SharKingStudios@users.noreply.github.com>
2026-01-21 08:37:05 -0500

    Change package from 'motor_teleop_demo' to 'terralift'

--- launch/fl_only.launch.py
+++ launch/fl_only.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='motor_teleop_demo',
+            package='terralift',
             executable='motor_test_node',
             output='screen',
             parameters=[{
