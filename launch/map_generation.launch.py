580403f1d166fccf59975615e3ec6d0ad4059f20
Logan Peterson <Brushfire257@gmail.com>
2026-03-03 22:30:06 -0500

    Bandwidth Reduction

--- launch/map_generation.launch.py
+++ launch/map_generation.launch.py
@@ -32,7 +32,7 @@ def generate_launch_description():
 
     slam_node = Node(
         package='slam_toolbox',
-        executable='sync_slam_toolbox_node',
+        executable='async_slam_toolbox_node',
         name='slam_toolbox',
         output='screen',
         parameters=[
